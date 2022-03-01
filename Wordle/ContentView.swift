601dc6c5b535a07bb523b0f78e91981f14b87157
Andre Dippenaar <andre.pierre.dippenaar@gmail.com>
2022-03-01 08:09:50 +0200

    Initial Commit

---
+++ Wordle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wordle
+//
+//  Created by Andre Dippenaar on 2022/03/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Andre Dippenaar on 2022/03/01.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
